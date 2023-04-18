77ff119c5610b46c3fc98695b665bd156f2182b3
BlueCannonBall <mmttiizz100@gmail.com>
2023-04-18 14:02:11 -0400

    fix fengine

--- src/position.cpp
+++ src/position.cpp
@@ -63,8 +63,7 @@ std::string Position::fen() const {
         << (history[game_ply].entry & WHITE_OOO_MASK ? "" : "Q")
         << (history[game_ply].entry & BLACK_OO_MASK ? "" : "k")
         << (history[game_ply].entry & BLACK_OOO_MASK ? "" : "q")
-        << (history[game_ply].entry & ALL_CASTLING_MASK ? "- " : "")
-        << (history[game_ply].epsq == NO_SQUARE ? " -" : SQSTR[history[game_ply].epsq]);
+        << (history[game_ply].entry & ALL_CASTLING_MASK ? "- " : "");
 
     return fen.str();
 }
